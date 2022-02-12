59fc3d8c1e17e47ffbcd6b587f8f84382e9a9284
MariKakuchaia <94448857+MariKakuchaia@users.noreply.github.com>
2022-02-12 19:20:53 +0400

    created a second section and added content

--- main.js
+++ main.js
@@ -18,3 +18,24 @@ function changeImg(){
     setTimeout("changeImg()", 5000);
 }
 window.onload=changeImg;
+
+function skillAnimationScroll() {
+    window.addEventListener("scroll", () => {
+        let skillsList = document.querySelector('.skills-list');
+        let skills = document.querySelectorAll(".skill");
+        let contentPosition = skillsList.getBoundingClientRect().top;
+        let screenPosition = window.innerHeight / 1.6;
+
+        if(contentPosition < screenPosition) {
+            for(let i = 0; i < skills.length; i++) {
+                skills[i].classList.add('active');
+            }
+        }else {
+            for(let i = 0; i < skills.length; i++) {
+                skills[i].classList.remove('active');
+            }
+        }
+    })
+}
+
+skillAnimationScroll()
